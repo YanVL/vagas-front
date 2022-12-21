9ff10e04b002000c69fc1141c95039680423fc12
YanVl <yanvitor.gti@gmail.com>
2022-12-20 22:02:36 -0300

    Adicionando registros em localstorage

--- src/components/views/PublicarVaga.vue
+++ src/components/views/PublicarVaga.vue
@@ -1,5 +1,33 @@
 <script>
+export default {
+  data: () => ({
+    titulo: '',
+    descricao: '',
+    modalidade: '',
+    tipo: '',
+  }),
+  methods: {
+    salvarVaga() {
 
+      let vagas = JSON.parse(localStorage.getItem('vagas'))
+
+      if(!vagas) vagas = []
+
+      let vaga = {
+        titulo: this.titulo,
+        descricao: this.descricao,
+        salario: this.salario,
+        modalidade: this.modalidade,
+        tipo: this.tipo
+      }
+
+      vagas.push(vaga)
+
+      localStorage.setItem('vagas', JSON.stringify(vagas))
+
+    }
+  }
+}
 </script>
 
 <template>
@@ -13,7 +41,7 @@
     <div class="row mt-3">
       <div class="col">
         <label class="form-label">Título da vaga</label>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="titulo">
         <div class="form-text">Por exemplo: Programador JavaScript e VueJs.</div>
       </div>
     </div>
@@ -21,7 +49,7 @@
     <div class="row mt-3">
       <div class="col">
         <label class="form-label">Descrição</label>
-        <textarea type="text" class="form-control"></textarea>
+        <textarea type="text" class="form-control" v-model="descricao"></textarea>
         <div class="form-text">Informe os detalhes da vaga.</div>
       </div>
     </div>
@@ -29,32 +57,34 @@
     <div class="row mt-3">
       <div class="col">
         <label class="form-label">Salário</label>
-        <input type="number" class="form-control">
+        <input type="number" class="form-control" v-model="salario">
         <div class="form-text">Informe o Salário</div>
       </div>
     </div>
 
-    <div class="col">
+    <div class="col mt-3">
       <label class="form-label">Modalidade</label>
-      <select class="form-select">
+      <select class="form-select" v-model="modalidade">
+        <option value="" disabled>--Selecione--</option>
         <option value="1">Home Office</option>
         <option value="2">Presencial</option>
       </select>
       <div class="form-text">Informe a modalidade de trabalho</div>
     </div>
 
-    <div class="col">
+    <div class="col mt-3">
       <label class="form-label">Tipo</label>
-      <select class="form-select">
+      <select class="form-select" v-model="tipo">
+        <option value="" disabled>--Selecione--</option>
         <option value="1">CLT</option>
         <option value="2">PJ</option>
       </select>
       <div class="form-text">Informe tipo de contratação</div>
     </div>
 
-    <div class="row">
+    <div class="row mt-3">
       <div class="col">
-        <button class="btn btn-primary">Cadastrar</button>
+        <button class="btn btn-primary" @click="salvarVaga()">Cadastrar</button>
       </div>
     </div>
 
